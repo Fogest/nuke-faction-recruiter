efe082594368f9c99ee8e4147a810eda336f8865
Justin Visser <justin@jhvisser.com>
2020-06-23 15:42:10 -0400

    Updating

--- nuke-recruiter-list.user.js
+++ nuke-recruiter-list.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Nuklear Applicant
 // @namespace    com.jhvisser.nuke
-// @version      1.0.0
+// @version      1.0.1
 // @description  To flag those who have had recruitment messages sent
 // @author       Altered By Fogest, Originally by Jox [1714547]
 // @match        https://www.torn.com/profiles.php*
 // @match        https://www.torn.com/hospitalview.php*
 // @match        https://www.torn.com/factions.php*
+// @match        https://www.torn.com/page.php*
 // @grant        GM_xmlhttpRequest
 // @connect      nuke.jhvisser.com
 // @connect      nukefamily.org
@@ -16,7 +17,7 @@
 (function() {
     'use strict';
 
-    var apiUrl = 'http://torn-faction-companies';
+    var apiUrl = 'http://torn-faction-companies/api';
 
     var uid = null;
     var fid = null;
@@ -29,7 +30,7 @@
     var factionName = null;
     var update = 1;
 
-    var blPrfileColor = 'rgba(255,0,0,0.3)';
+    var blPrfileColor = '#00a1ff3b';
     var blFactionColor = 'rgba(150,70,120,0.5)';
     var wlFactionColor = 'rgba(0,100,0,0.3)';
 
@@ -119,7 +120,7 @@
 
     function checkIsPlayerBlaklisted(){
 
-        var emptyBox = document.querySelector('.user-information');
+        var emptyBox = document.querySelector('.user-information .cont');
 
         if(!emptyBox){
             setTimeout(checkIsPlayerBlaklisted, 500);
@@ -132,7 +133,7 @@
 
             let facLink = document.querySelector(".basic-information .basic-list .user-information-section+div span a[href^='/factions.php']")
             fid = facLink ? facLink.href.replace('https://www.torn.com/factions.php?step=profile&ID=', '') : false;
-            factionName = facLink.text;
+            factionName = facLink ? facLink.text : false;
 
             if (id) {
                 TargetID = id;
@@ -141,29 +142,22 @@
             if (player) {
                 TargetPlayerName = player;
             }
+            //border-top-style: ridge;
+            //border-top-width: thin;
+            //padding-top: 4px;
+            //padding-bottom: 3px;
 
-            var a = document.createElement('a');
-            a.href="#";
-            a.innerHTML = "Add to Nuke's Recruit List";
-            a.style.margin = '1px 15px 0px';
-            //a.style.display = 'block';
-            a.onclick = reportToNuke;
+            var div = document.createElement('div');
+            div.classList.add('empty-box');
+            div.id = "recruit-list-box";
 
-            emptyBox.appendChild(a);
+            div.style.borderTop = 'thin ridge #cecece';
+            div.style.paddingTop = '4px';
+            div.style.paddingBottom = '3px';
 
-            var r = document.createElement('a');
-            r.href="#";
-            r.innerHTML = "Remove";
-            r.style.margin = '1px 15px 0px';
-            //r.style.display = 'block';
-            r.style.float = 'right';
-
-            emptyBox.appendChild(r);
-
-            var userInformationContainer = document.querySelector('.user-information');
+            var userInformationContainer = document.querySelector('.user-information .cont');
 
             if(blackList[id]){
-
                 var ul = document.createElement('ul');
                 ul.style.padding = '3px 10px';
                 ul.style.margin = '0 5px';
@@ -178,11 +172,29 @@
                 }
 
                 emptyBox.style.overflow = 'auto';
-                emptyBox.appendChild(ul);
+                div.appendChild(ul)
+                emptyBox.appendChild(div);
             }
             else{
-                profileContainer.style.backgroundColor = null;
+                var a = document.createElement('a');
+                a.href="#";
+                a.innerHTML = "Add to Nuke's Recruit List";
+                a.style.margin = '1px 15px 0px';
+                //a.style.display = 'block';
+                a.onclick = reportToNuke;
+
+                div.appendChild(a);
+
+                //var r = document.createElement('a');
+                //r.href="#";
+                //r.innerHTML = "Remove";
+                //r.style.margin = '1px 15px 0px';
+                //r.style.display = 'block';
+                //r.style.float = 'right';
+
+                //div.appendChild(r);
             }
+            emptyBox.appendChild(div);
         }
     }
 
@@ -196,14 +208,20 @@
             }
         }
 
+        const regex = /^(.*?)\s\[/;
+        PlayerName = regex.exec(PlayerName)[1];
+        TargetPlayerName = regex.exec(TargetPlayerName)[1];
 
         if(PlayerName){
             var postData = 'player_id=' + TargetID;
-            postData =+ '&faction_id=' + fid;
-            postData =+ '&recruited_by_id=' + getCookie('uid');
-            postData =+ '&player_name=' + TargetPlayerName;
-            postData =+ '&faction_name=' + factionName;
-            postData =+ '&recruited_by=' + PlayerName;
+            postData += '&recruited_by_id=' + getCookie('uid');
+            postData += '&player_name=' + TargetPlayerName;
+            postData += '&recruited_by=' + PlayerName;
+
+            if (fid && factionName) {
+                postData += '&faction_name=' + factionName;
+                postData += '&faction_id=' + fid;
+            }
 
             GM_xmlhttpRequest ( {
                 method:     'POST',
@@ -211,7 +229,7 @@
                 headers:    {"Content-Type": "application/x-www-form-urlencoded"},
                 data:       postData,
                 onload:     function (response) {
-                    alert(responseDetails.responseText);
+                    alert(response.responseText);
                     updateBlackList();
                 }
             });
